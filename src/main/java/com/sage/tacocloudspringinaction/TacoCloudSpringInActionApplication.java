a50df07ea145ec4c17846d3742ee4d6f0f408f0d
Satish Kumar Patra <psatishkumar720@gmail.com>
2023-01-22 08:50:29 +0530

    Initial commit

---
+++ src/main/java/com/sage/tacocloudspringinaction/TacoCloudSpringInActionApplication.java
@@ -0,0 +1,13 @@
+package com.sage.tacocloudspringinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudSpringInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudSpringInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sage/tacocloudspringinaction/TacoCloudSpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sage.tacocloudspringinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudSpringInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
