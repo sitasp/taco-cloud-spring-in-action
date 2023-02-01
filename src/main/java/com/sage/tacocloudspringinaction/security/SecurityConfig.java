de2cf3ea7d1ec5650cba79378985b4ab11b5138c
Satish Patra <psatishkumar720@gmail.com>
2023-02-01 22:33:38 +0530

    Ch04: In-memory user store used

--- src/main/java/com/sage/tacocloudspringinaction/security/SecurityConfig.java
+++ src/main/java/com/sage/tacocloudspringinaction/security/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.sage.tacocloudspringinaction.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig{
@@ -10,5 +13,15 @@ public class SecurityConfig{
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> {};
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
